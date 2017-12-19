f0a39cd559fc49c6f774b889bc662376837c4883
s147682 <tmrgast@live.nl>
2017-12-19 15:49:42 +0100

    html changes

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,6 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'floathub/explore.html', {'posts': posts})
+
+def home(request):
+    return render(request, 'floathub/index.html', {})
