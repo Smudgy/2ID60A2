67490bcd5e9cbc191fcb67bf8e4b3ae302fc71b6
s147682 <tmrgast@live.nl>
2017-12-20 21:08:42 +0100

    fix on user error

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,8 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(author=request.user).filter(published_date__lte=timezone.now()).order_by('-published_date')
+    #posts = Post.objects.filter(author=request.user).filter(published_date__lte=timezone.now()).order_by('-published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'floathub/base.html', {'posts': posts})
 
 def post_new(request):
