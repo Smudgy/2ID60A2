0d3ad60a185b8f45a21dffa7ac7b34651f9e6637
s147682 <tmrgast@live.nl>
2017-12-21 23:17:53 +0100

    rest api

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 ]
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.home, name='start'),
     #url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^home/$', views.home, name='home'),
     url(r'^notes/$', views.post_list, name='post_list'),
--- python_intro.py
+++
@@ -1,8 +0,0 @@
-def hi(n):
-    print('Hi ' + girls[n] + '!')
-
-girls = ['Rachel', 'Monica', 'Phoebe', 'Alina', 'You']
-for i in range(len(girls)):
-    hi(i)
-    if i < len(girls) - 1:
-        print('Next girl')
